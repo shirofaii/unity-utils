4b52da89d7225f236b8e93f5288e7d04a6047b96
Egor Skorik <e.skorik@chillgaming.net>
2026-05-05 23:29:19 +0400

    rect extension

---
+++ Assets/root/Runtime/RectExtensions.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace SharedUtils
+{
+    public static class RectExtensions
+    {
+        public static Rect Padding(this Rect rect, float padding)
+        {
+            rect.x += padding;
+            rect.y += padding;
+            float num = padding * 2f;
+            rect.width -= num;
+            rect.height -= num;
+            return rect;
+        }
+    }
+}
